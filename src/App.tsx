338de09255679f6fd8fc8b19efe2c689df1bf230
Christopher Lye <clye@palo-it.com>
2023-06-06 16:07:04 +0800

    Add simple fetch call to retrieve accordions

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {useState, useEffect} from 'react';
+
+interface Accordion {
+    title: string;
+    content: string;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [accordions, setAccordions] = useState<Accordion[]>([]);
+
+    useEffect(() => {
+        const getData = async () => {
+            const response = await fetch('http://167.172.80.70:8055/items/acc001_application_status_and_processes');
+            const jsonData = await response.json();
+            setAccordions(jsonData.data);
+        }
+        getData();
+    }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>POC FE UI</h1>
+        {
+            accordions.length > 0 ? accordions.map(a => {
+                return (
+            <div>
+            <p>{a.title}</p>
+            <p>{a.content}</p>
+            </div>
+            )
+        }) : <h1>No accordions</h1>
+        }
+
     </>
   )
 }
