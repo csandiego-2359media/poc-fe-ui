a523729da07e803a7cb75ce95b800249345528bf
Christopher Lye <clye@palo-it.com>
2023-06-08 14:03:23 +0800

    Add render based on pathname, uuid search, preview if not published
    - Render based on pathname: allows agency user to render content only if permalink matches pathname
    - UUID search: allows agency user to copy UUID of a particular component to perform a search and locate it on the CMS
    - Preview if not published: allows preview in CMS to display the page even if content is not published yet

--- src/App.tsx
+++ src/App.tsx
@@ -4,28 +4,39 @@ import {useState, useEffect} from 'react';
 interface Accordion {
     title: string;
     content: string;
+    uuid: string;
+    permalink: string;
 }
 
 function App() {
     const [accordions, setAccordions] = useState<Accordion[]>([]);
+    const params = new URLSearchParams(window.location.href);
+    const status = params.get('status');
 
     useEffect(() => {
+        console.log(window.location.pathname);
         const getData = async () => {
-            const response = await fetch('http://167.172.80.70:8055/items/acc001_application_status_and_processes');
+            // const filter = `?filter={ "status": { "_eq": "published" }}`
+            const response = await fetch(`http://167.172.80.70:8055/items/ssg_accordion`);
             const jsonData = await response.json();
             setAccordions(jsonData.data);
         }
         getData();
     }, [])
 
+    const handleGetUuid = (e: any) => {
+        console.log('handleGetUuid', e.target.dataset.uuid);
+        navigator.clipboard.writeText(e.target.dataset.uuid);
+    }
+
   return (
     <>
       <h1>POC FE UI</h1>
         {
-            accordions.length > 0 ? accordions.map(a => {
+            accordions.length > 0 ? accordions.filter(a => (a.permalink === window.location.pathname && status !== 'published')).map(a => {
                 return (
             <div>
-            <p>{a.title}</p>
+            <p data-uuid={a.uuid} onClick={handleGetUuid}>{a.title}</p>
             <p>{a.content}</p>
             </div>
             )
