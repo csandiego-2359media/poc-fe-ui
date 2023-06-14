1b5df0e39ec6e5510a1f57ec400df8d4960e3eb9
Christopher San Diego <christopher.sandiego@2359media.com>
2023-06-14 16:46:30 +0800

    Add gobiz styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,60 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 import { Directus } from "@directus/sdk";
+import { Header, Menu, Button } from "@molb/gobiz-styleguide";
+import styled from "styled-components";
+import { MenuOption } from "@molb/gobiz-styleguide/src/components/menu/components/MenuHeader";
+
+const HeaderContent = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const TitleSection = styled.section`
+  background-color: #037e8a;
+  color: #ffffff;
+  padding: 36px 48px 36px 48px;
+
+  nav > ul {
+    display: flex;
+    flex-grow: 1;
+    flex-shrink: 0;
+    justify-content: flex-start
+    list-style-type: none;
+  }
+
+  nav > ul > li {
+    display: flex;
+  }
+
+  nav > ul > li+li::before {
+    content: "/";
+    padding: 10px 0px;
+  }
+
+  nav > ul > li > a {
+    color: #ffffff;
+    display: block;
+    font-size: 15px;
+    padding: 10px 15px;
+    text-decoration: none;
+  }
+
+  nav > ul > li:first-child > a {
+    padding: 10px 15px 10px 0px;
+  }
+
+  nav > ul > li > a:hover {
+    color: #d6d6d6;
+  }
+
+  h1 {
+    font-family: hknova-semibold;
+    font-size: 48px;
+    font-weight: 600;
+    margin-top: 24px;
+  }
+`;
 
 interface Accordion {
   title: string;
@@ -14,6 +68,73 @@ function App() {
   const params = new URLSearchParams(window.location.href);
   const status = params.get("status");
   const directus = new Directus(import.meta.env.DIRECTUS_SERVER);
+  const optionList: MenuOption[] = [{
+    key: '/',
+    label: 'Start a Business',
+    icon: 'Licence',
+    submenu: [
+      {
+        key: "/",
+        label: "Sample",
+        sublevel: false,
+      },
+    ],
+  }, {
+    key: '/',
+    label: 'Run and Grow a Business',
+    icon: 'Licence',
+    submenu: [
+      {
+        key: "/",
+        label: "Sample",
+        sublevel: false,
+      },
+    ],
+  }, {
+    key: '/',
+    label: 'Covid-19',
+    icon: 'Licence',
+    submenu: [
+      {
+        key: "/",
+        label: "Sample",
+        sublevel: false,
+      },
+    ],
+  }, {
+    key: '/',
+    label: 'e-Services',
+    icon: 'Licence',
+    submenu: [
+      {
+        key: "/",
+        label: "Sample",
+        sublevel: false,
+      },
+    ],
+  }, {
+    key: '/',
+    label: 'Resources',
+    icon: 'Licence',
+    submenu: [
+      {
+        key: "/",
+        label: "Sample",
+        sublevel: false,
+      },
+    ],
+  }, {
+    key: '/',
+    label: 'About Us',
+    icon: 'Licence',
+    submenu: [
+      {
+        key: "/",
+        label: "Sample",
+        sublevel: false,
+      },
+    ],
+  }];
 
   useEffect(() => {
     const getData = async () => {
@@ -36,8 +157,25 @@ function App() {
     navigator.clipboard.writeText(e.target.dataset.uuid);
   };
 
+  const nav = <HeaderContent>
+    <Menu optionList={optionList} selectedKeys={[]} onClick={() => {}} mode="horizontal"/>
+    <Button label="Login" size="medium" />
+  </HeaderContent>;
+
   return (
     <>
+      <Header childComponent={nav}/>
+      <TitleSection>
+        <nav>
+          <ul>
+            <li><a href="#">HOME</a></li>
+            <li><a href="#">EPJS</a></li>
+            <li><a href="#">PROGRAMMES AND INITIATIVES</a></li>
+            <li><a href="#">DEVELOP HUMAN CAPITAL</a></li>
+          </ul>
+        </nav>
+        <h1>Develop Human Capital</h1>
+      </TitleSection>
       <h1>POC FE UI</h1>
       {accordions.length > 0 ? (
         accordions
